dfff9bf024b69ced7e49e051565a5d6469e65c87
wilferraciolli <wiliam334@hotmail.com>
2018-02-01 12:59:35 +0200

    Added a ApplicationRunner to auto populate some data on the database.

--- src/main/java/com/wiltech/DemoApplication.java
+++ src/main/java/com/wiltech/DemoApplication.java
@@ -1,7 +1,13 @@
 package com.wiltech;
 
+import com.wiltech.car.Car;
+import com.wiltech.car.CarRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 /**
  * The type Demo application.
@@ -16,5 +22,27 @@ public class DemoApplication {
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	/**
+	 * Adding  ApplicationRunner bean to the Application Boot to run set up data on the database.
+	 *
+	 * @param repository the repository
+	 * @return application runner
+	 */
+	@Bean
+	ApplicationRunner init(CarRepository repository) {
+		//Populate a list of cars on the database
+		return args -> {
+			Stream.of("Ferrari", "Jaguar", "Porsche", "Lamborghini", "Bugatti",
+					"AMC Gremlin", "Triumph Stag", "Ford Pinto", "Yugo GV").forEach(name -> {
+				Car car = new Car();
+				car.setName(name);
+				repository.save(car);
+			});
+
+			//Print every car on the database
+			repository.findAll().forEach(System.out::println);
+		};
 	}
 }
