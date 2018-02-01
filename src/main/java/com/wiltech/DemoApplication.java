c687b6efbc35ed536c169d0c2393f4261436ffa2
wilferraciolli <wiliam334@hotmail.com>
2018-02-01 12:10:16 +0200

    Initial Commit

---
+++ src/main/java/com/wiltech/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.wiltech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Demo application.
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
