2c7384113da25347f0a9347f0e51c0a1052eff31
wilferraciolli <wiliam334@hotmail.com>
2018-03-05 22:57:51 +0000

    Added oAuth on to the back end.

--- src/main/java/com/wiltech/DemoApplication.java
+++ src/main/java/com/wiltech/DemoApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import java.util.stream.Stream;
 
 /**
  * The type Demo application.
  */
+@EnableResourceServer
 @SpringBootApplication
 public class DemoApplication {
 
